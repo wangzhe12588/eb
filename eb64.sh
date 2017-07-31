ce49a7c7e0b137c42f2438c5f2d547bb3dd96148
wangzhe12588 <wangzhe12588@gmail.com>
2017-07-31 14:55:59 +0800

    Update eb64.sh

--- eb64.sh
+++ eb64.sh
@@ -1,7 +1,8 @@
 apt-get update
 apt-get -q -y --force-yes install tightvncserver xterm jwm mercurial libasound2-dev libcurl4-openssl-dev libnotify-dev libxt-dev libiw-dev mesa-common-dev autoconf2.13 yasm libidl-dev screen unzip bzip2 nano
 wget https://download-installer.cdn.mozilla.net/pub/firefox/releases/54.0.1/linux-x86_64/en-US/firefox-54.0.1.tar.bz2
-wget http://wyc.buzz/eb/vncxstartup.txt http://wyc.buzz/eb/vncserverinit.txt
+wget -N --no-check-certificate https://raw.githubusercontent.com/wangzhe12588/eb/master/vncxstartup.txt
+wget -N --no-check-certificate https://raw.githubusercontent.com/wangzhe12588/eb/master/vncserverinit.txt
 tar xjf firefox-54.0.1.tar.bz2
 mv firefox/ /usr/local/lib/
 ln -s /usr/local/lib/firefox/firefox /usr/bin/firefox
